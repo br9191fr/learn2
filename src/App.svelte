1e850db35517f281b20f686a6da0b8cb50b2a69a
Bruno Ricci <br9191fr@gmail.com>
2023-12-27 10:21:52 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+    let products = [
+        {
+            id: 'p1',
+            title: 'A newbook',
+            price: '19.99'
+        }
+    ];
+    import Product from "./Product.svelte";
+
+    function addToCard(event) {
+        console.log(event.detail);
+    }
+    function deleteProduct(event) {
+        console.log(event.detail);
+    }
+</script>
+{#each products as product}
+<Product title={product.title}
+         price={product.price}
+         on:add-to-card={addToCard}
+         on:delete={deleteProduct}
+/>
+{/each}
+
+
+
+
---
+++ src/Product.svelte
@@ -0,0 +1,18 @@
+<script>
+    import {createEventDispatcher} from 'svelte';
+    export let title;
+    export let price
+
+    const dispatch = createEventDispatcher()
+
+    function addToCard() {
+        dispatch('add-to-card', {id: 'p1'});
+    }
+</script>
+
+<article>
+    <h1>{title}</h1>
+    <h2>${price}</h2>
+    <button on:click="{addToCard}">Add to Card</button>
+    <button on:click="{() => dispatch('delete', 'p1')}">Delete</button>
+</article>
