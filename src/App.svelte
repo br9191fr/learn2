c22aa33d8bc0daac264516a858462113a3e23ca7
Bruno Ricci <br9191fr@gmail.com>
2023-12-27 12:18:56 +0100

    after 61

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,7 @@
 <script>
+    import Product from "./Product.svelte";
+    import Modal from "./Modal.svelte";
+
     let products = [
         {
             id: 'p1',
@@ -6,22 +9,25 @@
             price: '19.99'
         }
     ];
-    import Product from "./Product.svelte";
 
     function addToCard(event) {
         console.log(event.detail);
     }
+
     function deleteProduct(event) {
         console.log(event.detail);
     }
 </script>
 {#each products as product}
-<Product {...product}
-         on:add-to-card={addToCard}
-         on:delete={deleteProduct}
-/>
+    <Product {...product}
+             on:add-to-card={addToCard}
+             on:delete={deleteProduct}
+    />
 {/each}
 
-
+<Modal>
+ <h1>Hello</h1>
+    <h2>Bruno</h2>
+</Modal>
 
 
